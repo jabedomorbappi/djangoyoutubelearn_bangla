52167be0b99548039892263afbac2b1787f43880
Jabed <jabedomorbappi100@gmail.com>
2024-09-02 21:59:04 +0600

    add and modify basic.html and contact

--- Firstproject/Helloworld/Helloworld/urls.py
+++ Firstproject/Helloworld/Helloworld/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home,name='home'),
+    path('contact/',contact,name='contact'),
+    
 ]
--- Firstproject/Helloworld/Helloworld/views.py
+++ Firstproject/Helloworld/Helloworld/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render,HttpResponse
 
 def home(request):
-    name=['jabed','omor','bappi']
-    context={'name':name}
-    return render(request,'home.html',context)
+    if request.method=="GET":
+        
+        name=['jabed','omor','bappi']
+        context={'name':name}
+        return render(request,'home.html',context)
 
 
+def contact(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        phone=request.POST['phone']
+        content=request.POST['contact']
+        print(name)
+        print(phone)
+        print(content)
+    return render(request,'contact.html')    
+        
+        
+    
