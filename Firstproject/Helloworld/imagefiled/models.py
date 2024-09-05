277f51d344f587b63ef116f39ec783389385ff27
Jabed <jabedomorbappi100@gmail.com>
2024-09-05 23:08:32 +0600

    Save current changes before reverting to previous commit

--- Firstproject/Helloworld/imagefiled/models.py
+++ Firstproject/Helloworld/imagefiled/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from PIL import Image
 from django.utils.text import slugify
+from multiselectfield import MultiSelectField
 
 class ImageDescription(models.Model):
+    MEDIUM=(
+        ('bangla','bangla'),
+        ('english','english'),
+        ('french','french'),
+        ('Hhindi','hindi'),
+        ('german','german'),
+        )
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=101)
     slug = models.CharField(max_length=100, unique=True, blank=True)
     image = models.ImageField(default='defaults.jpg', upload_to='images/')
+    medium=MultiSelectField(default='bangla',max_length=100,max_choices=5,choices=MEDIUM)
 
     def __str__(self):
         return self.name
