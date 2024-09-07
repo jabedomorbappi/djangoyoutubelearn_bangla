2914f825afa8842d263e8c29fa7bbcce17a9449b
Jabed <jabedomorbappi100@gmail.com>
2024-09-07 16:04:57 +0600

    remove the imagefiled

--- Firstproject/Helloworld/imagefiled/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ImageDescription
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = ImageDescription
-        fields = '__all__'
--- Firstproject/Helloworld/imagefiled/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import imageshow,loadimage
-
-urlpatterns = [
-
-    path('posts/',imageshow,name='imageshow'),
-    path('load_image/',loadimage,name='load_image'),
-    
-]
